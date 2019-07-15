f57d20d1146891bd5518cb71ded246cc48ebbf14
Heather deManbey <h.demanbey@gmail.com>
2019-07-15 11:52:34 -0400

    Updates

---
+++ precache-manifest.968ed497866c7c548dc9a71193e54710.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f370ed240a0a95dfacebed87e51baf4",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "b5477b78b1138410be48",
+    "url": "/monsters-react/static/css/main.ac7b9a1e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b5477b78b1138410be48",
+    "url": "/monsters-react/static/js/main.5699be84.chunk.js"
+  },
+  {
+    "revision": "e94f20c01308e085c048",
+    "url": "/monsters-react/static/js/runtime~main.56780d6f.js"
+  }
+]);
